d91865139cea6a983c926699d4aa396f8b71306d
Irene <73439899+irenechenn@users.noreply.github.com>
2023-07-04 16:35:49 -0700

    try6

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to My Home Page</h1>
+      <p>This is a simple home page created using React.</p>
+    </div>
+  );
+};
+
+export default HomePage;
